3a4f8d97d3eef29dda312f602d9a0ede30d77ca3
Allex-Nik <alexandr.nikolaev96@gmail.com>
2025-05-29 19:34:46 +0200

    Add buildVisitor() and isApplicableByPsi() in AddColumnInspection. Change name in plugin.xml to avoid the word plugin

--- src/main/kotlin/com/github/allexnik/dataframequickfixplugin/inspections/AddColumnInspection.kt
+++ src/main/kotlin/com/github/allexnik/dataframequickfixplugin/inspections/AddColumnInspection.kt
@@ -6,8 +6,15 @@ import org.jetbrains.kotlin.analysis.api.KaSession
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinApplicableInspectionBase
 import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinModCommandQuickFix
+import org.jetbrains.kotlin.idea.intentions.callExpression
+import org.jetbrains.kotlin.idea.intentions.calleeName
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtNameReferenceExpression
 import org.jetbrains.kotlin.psi.KtQualifiedExpression
+import org.jetbrains.kotlin.psi.KtStringTemplateExpression
 import org.jetbrains.kotlin.psi.KtVisitor
+import org.jetbrains.kotlin.psi.qualifiedExpressionVisitor
 
 
 class AddColumnInspection : KotlinApplicableInspectionBase.Simple<KtQualifiedExpression, Unit>() {
@@ -20,11 +27,27 @@ class AddColumnInspection : KotlinApplicableInspectionBase.Simple<KtQualifiedExp
     }
 
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): KtVisitor<*, *> {
-        TODO()
+        return qualifiedExpressionVisitor { qualifiedExpression ->
+            visitTargetElement(qualifiedExpression, holder, isOnTheFly)
+        }
     }
 
     override fun isApplicableByPsi(element: KtQualifiedExpression): Boolean {
-        TODO()
+        val selector = element.selectorExpression as? KtCallExpression ?: return false
+        val calleeExpr = selector.calleeExpression as? KtNameReferenceExpression ?: return false
+        if (calleeExpr.getReferencedName() != "add") return false
+
+        val arguments = selector.valueArguments
+        if (arguments.size != 1) return false
+
+        val firstArg = arguments.first().getArgumentExpression() ?: return false
+        if (firstArg !is KtStringTemplateExpression) return false
+
+        val lambda = selector.lambdaArguments.singleOrNull()?.getLambdaExpression() ?: return false
+        if (lambda.bodyExpression == null) return false
+
+//        return lambda.bodyExpression?.statements?.any { it.text.contains("from") } != true
+        return true
     }
 
     override fun KaSession.prepareContext(element: KtQualifiedExpression): Unit? {
